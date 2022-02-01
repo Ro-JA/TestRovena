798f93a3f1c49ea0d9dad02f7c24b45d060250f6
Roman <boyra75.rb@gmail.com>
2022-02-01 21:11:00 +0900

    add var button

--- app/src/main/java/com/ro_ja/android/testrovena/MainActivity.kt
+++ app/src/main/java/com/ro_ja/android/testrovena/MainActivity.kt
@@ -2,8 +2,20 @@ package com.ro_ja.android.testrovena
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var button4: Button
+    private lateinit var button5: Button
+    private lateinit var button6: Button
+    private lateinit var buttonRight: Button
+    private lateinit var buttonLeft: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
