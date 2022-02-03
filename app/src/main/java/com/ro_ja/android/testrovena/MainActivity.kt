323739843f8557fa7129bb972345b7b5d7833799
Roman <boyra75.rb@gmail.com>
2022-02-03 21:25:54 +0900

    imageButton

--- app/src/main/java/com/ro_ja/android/testrovena/MainActivity.kt
+++ app/src/main/java/com/ro_ja/android/testrovena/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.ro_ja.android.testrovena
 
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
-    private lateinit var button1: Button
-    private lateinit var button2: Button
-    private lateinit var button3: Button
-    private lateinit var button4: Button
-    private lateinit var button5: Button
-    private lateinit var button6: Button
-    private lateinit var buttonRight: Button
-    private lateinit var buttonLeft: Button
+    private lateinit var button1: ImageButton
+    private lateinit var button2: ImageButton
+    private lateinit var button3: ImageButton
+    private lateinit var button4: ImageButton
+    private lateinit var button5: ImageButton
+    private lateinit var button6: ImageButton
+    private lateinit var buttonRight: ImageButton
+    private lateinit var buttonLeft: ImageButton
     private var numberButton = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
