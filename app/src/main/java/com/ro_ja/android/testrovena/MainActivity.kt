1d24a1336e0d9cb3e8da7d92649f0a4122a66f27
Roman <boyra75.rb@gmail.com>
2022-02-06 18:42:59 +0900

    add toast.button

--- app/src/main/java/com/ro_ja/android/testrovena/MainActivity.kt
+++ app/src/main/java/com/ro_ja/android/testrovena/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.ro_ja.android.testrovena
 
-import android.media.Image
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
